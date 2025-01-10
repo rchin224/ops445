a4a2e7e81077509c8614cc89cdd9f8555627e66c
Roye Chin <rchin11@myseneca.ca>
2025-01-10 09:47:36 -0500

    Added lab1c

---
+++ lab1/lab1c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+num1 = 5
+num2 = 10
+#print (num1) 
+#print (num2)
+sum = num1 + num2
+#print (sum)
+#print ('The sum is: ' + str(sum))
+
+#!/usr/bin/env python3
+
+# Define the required objects
+name = "Isaac"
+age = 72
+
+# Ensure the output matches the exact requirement
+print(f"{name} is {age} years old!")
